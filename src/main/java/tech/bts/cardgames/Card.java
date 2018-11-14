613fc72916981cf5b02111dea34982d19edccc95
vanessahigasi <vanessahigasi@gmail.com>
2018-11-14 11:29:39 +0100

    Create Card and Deck

---
+++ src/main/java/tech/bts/cardgames/Card.java
@@ -0,0 +1,31 @@
+package tech.bts.cardgames;
+
+public class Card {
+
+    private int magic;
+    private int strength;
+    private int intelligence;
+
+    public Card(int magic, int strength, int intelligence) {
+        this.magic = magic;
+        this.strength = strength;
+        this.intelligence = intelligence;
+    }
+
+    public int getMagic() {
+        return magic;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    @Override
+    public String toString() {
+        return "M:" + magic + ", S:" + strength + ", I:" + intelligence;
+    }
+}
---
+++ src/main/java/tech/bts/cardgames/Deck.java
@@ -0,0 +1,73 @@
+package tech.bts.cardgames;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Deck {
+
+    private List<Card> cards;
+
+    public Deck() {
+        this.cards = new ArrayList<Card>();
+    }
+
+    public void add(Card card) {
+        this.cards.add(card);
+    }
+
+    public void generate() {
+
+        for (int m = 1; m <= 8; m++) {
+
+            for (int s = 1; s <= 9 - m; s++) {
+                int i = 10 - (m+s);
+                this.add(new Card(m, s, i));
+            }
+        }
+    }
+    public void shuffle() {
+
+        System.out.println("Shuffling " + this.cards.size() + " cards");
+
+        Random random = new Random();
+
+        /*
+        int index1 = 0;
+        int index2 = random.nextInt(this.cards.size()); //random index in cards
+
+
+        Card card1 = this.cards.get(index1);
+        Card card2 = this.cards.get(index2);
+
+        this.cards.set(index2,card1); //change positions
+        this.cards.set(index1,card2);
+        */
+
+        for (int i = 0; i < this.cards.size() -1; i++) {
+
+            //pick a random index and swap it with card at index 'i'
+
+            int randomIndex = random.nextInt(this.cards.size()); //random index after index i
+
+             // int randomIndex = random.nextInt(this.cards.size()); //random index
+
+            System.out.println("Swapping cards at indexes " + i + " and " + randomIndex);
+
+            Card card1 = this.cards.get(i);
+            Card card2 = this.cards.get(randomIndex);
+
+            this.cards.set(randomIndex,card1); //change positions
+            this.cards.set(i,card2);
+
+
+        }
+
+    }
+
+    public Card pickCard() {
+
+        Card card = cards.remove(cards.size() - 1); //removes the last card
+        return card;
+    }
+}
