dcebcaa24527e1e6187e70b5bbc380f376775c42
vanessahigasi <vanessahigasi@gmail.com>
2018-11-16 10:42:59 +0100

    added State enum

---
+++ src/main/java/tech/bts/cardgames/Hand.java
@@ -0,0 +1,41 @@
+package tech.bts.cardgames;
+
+import java.util.List;
+
+public class Hand {
+
+
+    private List<Card> cards;
+    private Hand hand1;
+    private Hand hand2;
+
+    public Hand(Hand hand1, Hand hand2) {
+        this.hand1 = hand1;
+        this.hand2 = hand2;
+    }
+
+    public Hand(List<Card> cards) {
+
+        this.cards = cards;
+
+    }
+
+    public Card calculate() {
+
+        int magic = 0;
+        int strength = 0;
+        int intelligence = 0;
+
+        for (Card card : cards) {
+            magic += card.getMagic();
+            strength += card.getStrength();
+            intelligence += card.getIntelligence();
+        }
+        return new Card(magic, strength, intelligence);
+    }
+
+    @Override
+    public String toString() {
+        return "Hand 1 is:" + hand1 + " and " + "Hand 2 is: " + hand2;
+    }
+}
